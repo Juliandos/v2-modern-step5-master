611dd4ca37d227fde9dafdc3f8f1f33ab7cad35e
Julian David Ortega Solarte <95juliandos@gmail.com>
2025-10-27 11:32:17 -0500

    asd

--- app/server.py
+++ app/server.py
@@ -22,7 +22,8 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000"  # React development server
+        "http://localhost:3000",
+        "http://localhost:3001",
     ],
     allow_credentials=True,
     allow_methods=["*"],
